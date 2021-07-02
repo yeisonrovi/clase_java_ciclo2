fcc9f724a6a4bfd8f082145d1c439a41120a65ab
yeisonrovi <yrviuche@gmail.com>
2021-07-01 19:08:05 -0500

    Agregué mis primeras lineas

--- Proyecto Inicial/src/clasesjava_ciclo2.java
+++ Proyecto Inicial/src/clasesjava_ciclo2.java
@@ -2,6 +2,7 @@ public class clasesjava_ciclo2 {
     public static void main (String[] args){
         System.out.println("Hola mundo!");
         System.out.println("Hola yeison!");
+        System.out.println("Hola Sofii!");
     }
     
 }
