9aa9b4ffa528cb3c892702eb04c44033dc9844e7
yeisonrovi <yrviuche@gmail.com>
2021-07-01 19:15:36 -0500

    ingrese unas lineas

--- Proyecto Inicial/src/clasesjava_ciclo2.java
+++ Proyecto Inicial/src/clasesjava_ciclo2.java
@@ -3,6 +3,8 @@ public class clasesjava_ciclo2 {
         System.out.println("Hola mundo!");
         System.out.println("Hola yeison!");
         System.out.println("Hola Sofii!");
+        System.out.println("Hola Sofii2!");
+        System.out.println("Hola Sofii3!");
     }
     
 }
