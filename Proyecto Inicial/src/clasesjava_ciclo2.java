761e2209034157a388f4427cfdbe4a74cde9ef50
yeisonrovi <yrviuche@gmail.com>
2021-07-01 19:02:48 -0500

    agregué una nueva linea

--- Proyecto Inicial/src/clasesjava_ciclo2.java
+++ Proyecto Inicial/src/clasesjava_ciclo2.java
@@ -1,6 +1,7 @@
 public class clasesjava_ciclo2 {
     public static void main (String[] args){
         System.out.println("Hola mundo!");
+        System.out.println("Hola yeison!");
     }
     
 }
